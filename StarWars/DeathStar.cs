d76ad33a3c399b7409e58d52f4e39b9bf3bff342
Tural Taghiyev <turalrt@code.edu.az>
2021-12-19 22:20:01 +0400

    first commit

---
+++ StarWars/DeathStar.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StarWars
+{
+    public class DeathStar : SpaceStation
+    {
+        public override void FireLaser()
+        {
+            Console.WriteLine("Pew pew");
+        }
+    }
+}
---
+++ StarWars/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace StarWars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DeathStar deathStar = new DeathStar();
+            deathStar.FireLaser();
+        }
+    }
+}
